120c41020cf254d2203132a43cf401dcdb9a033c
Roberto Palma <rpalmattu@gmail.com>
2023-05-05 11:23:00 -0700

    Add project files.

---
+++ ToolProgramCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToolProgramCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
