bf2be49e044612b26bc3cb6dbae05c14df914a9a
Roberto Palma <rpalmattu@gmail.com>
2023-05-08 14:21:17 -0700

    Measure change and add

---
+++ ToolProgramCore/Models/ToolMeasure.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ToolProgramCore.Models
+{
+    public class ToolMeasure
+    {
+        [Key]
+        public string ? ID { get; set; }
+
+        // Added in DataLibrary
+        [Display(Name = "Date Borrowed ")]
+        [DisplayFormat(DataFormatString = " {0:MM/dd/yyyy hh:mm tt}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.DateTime)]
+        public DateTime? T_Date { get; set; }
+
+        [Display(Name = "Work Center")]
+        [Required]
+        public String? WC { get; set; }
+
+        [Required]
+        [Display(Name = "Tool Number")]
+        public string? ToolNo { get; set; }
+
+        [Display(Name = "Employee Number")]
+        [Required]
+        public String? EmpNo { get; set; }
+
+        [Display(Name = "Standard Size")]
+        [Required]
+        public String? S_Size { get; set; }
+
+
+        [Display(Name = "Standard Deviation")]
+        [Required]
+        public String? Condition { get; set; }
+
+        public List<string>? WCdropDownList { get; set; }
+        public List<string>? EmplDropDownList { get; set; }
+        public List<string>? ToolNoDropDownList { get; set; }
+    }
+}
