526939df132c787915022a12484e55957a05d1d0
Roberto Palma <rpalmattu@gmail.com>
2023-05-08 13:32:34 -0700

    Added new class library for data and added models

---
+++ DataLibrary/Models/ToolMeasureModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models
+{
+    internal class ToolMeasureModel
+    {
+        [Key]
+
+        public String ? ID { get; set; }
+
+        public String ? T_Date { get; set; }
+
+        public String ? WC { get; set; }
+
+        public string? ToolNo { get; set; }
+
+        public string ? S_Size { get; set; }        
+
+        public String ? EmpNo { get; set; }
+
+        public String ? condition { get; set; }
+
+        
+    }
+}
---
+++ DataLibrary/Models/ToolReturnModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models
+{
+    internal class ToolReturnModel
+    {
+        [Key]
+
+        public String? toolNumber { get; set; }
+
+        public string? D_Remove { get; set; }
+
+        public String? P_Return { get; set; }
+
+        public String? WC { get; set; }
+
+        public String? EmpNo { get; set; }
+
+        public String? DateReturned { get; set; }
+
+        public String? ID { get; set; }
+    }
+}
