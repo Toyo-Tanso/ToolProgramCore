54879879f74c103e45dab6c951ed7acf7d22cc99
Roberto Palma <rpalmattu@gmail.com>
2023-06-12 11:54:17 -0700

    Making it so the same structure is there for other implementations

--- ToolProgramCore/Controllers/AdminChanging/WorkCenterController.cs
+++ ToolProgramCore/Controllers/AdminChanging/WorkCenterController.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ToolProgramCore.Models;
 
 namespace ToolProgramCore.Controllers.AdminChanging
 {
     public class WorkCenterController : Controller
     {
+        // TODO: make a toast that notifies success on all pages
+
+
         // GET: WorkCenterController
         public ActionResult Index()
         {
-            return View();
+            GetWCList();
+
+            return View(WorkCenterList);
         }
 
         // GET: WorkCenterController/Details/5
@@ -17,25 +24,55 @@ namespace ToolProgramCore.Controllers.AdminChanging
             return View();
         }
 
-        // GET: WorkCenterController/Create
-        public ActionResult Create()
+        // GET: WorkCenterController/AddWC
+        public ActionResult AddWC()
         {
             return View();
         }
 
-        // POST: WorkCenterController/Create
+        // POST: WorkCenterController/AddWC
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult AddWC(IFormCollection collection)
         {
             try
             {
+                AddWC(collection);
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
                 return View();
             }
+        }
+
+        // TODO: change description
+        // [Helper Function] Takes out values and uses the MeasuerLogic Controller to
+        // add tool check in
+        // TODO: Implement / change description
+        private void AddWCHelper(IFormCollection collection)
+        {
+
+            //string FirstName = collection["FirstName"].ToString();
+            //string LastName = collection["LastName"].ToString();
+            //string Clock_Code = collection["Clock_Code"];
+
+            //// Clock_Code will be unique because of model class data annotation
+            //// combine first and last name
+            //string fullName = LastName.Trim() + ", " + FirstName.Trim();
+
+            //// ensure length is less than 50
+            //// shouldn't be activated but just in case
+            //if (fullName.Length > 50)
+            //{
+            //    throw new Exception("Name too long");
+            //}
+
+            //// enter the new employee with active status
+            //// call to data library
+            //AddEmployeeDL(fullName, Clock_Code);
+            throw new NotImplementedException();
+
         }
 
         // GET: WorkCenterController/Edit/5
@@ -79,5 +116,40 @@ namespace ToolProgramCore.Controllers.AdminChanging
                 return View();
             }
         }
+
+
+        // ** Data Loaders **
+
+        // holds current list
+        public List<WorkCenter>? WorkCenterList;
+
+        [AllowAnonymous]
+        // TODO: remove nonymous and update description
+        // Gets the employee list and stores them in a new list with local Employee class
+        // This loads each time the index runs, gets new values every refresh
+        // TODO: IMPLEMENT
+        public void GetWCList()
+        {
+
+            //List<List<string>> data = LoadFields_dbl_lst("EMP");
+            //List<Employee> employeesTemp = new();
+
+            //// row = [name, Clock] 
+            //foreach (List<string> row in data)
+            //{
+            //    // Enter values into this model: Employee. Then add to list
+            //    employeesTemp.Add(new Employee
+            //    {
+            //        FirstName = row[0].Split(',')[1].Trim(),
+            //        LastName = row[0].Split(',')[0].Trim(),
+            //        FullName = row[0],
+            //        Clock_Code = row[1]
+
+            //    });
+            //}
+            //employeeList = employeesTemp;
+            throw new NotImplementedException();
+        }
+
     }
 }
